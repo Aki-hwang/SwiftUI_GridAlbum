39243e8d6e251c34bde3cabfaa8d5004b52d8ec8
chuljin Hwang <chljin.hwang@gmail.com>
2022-01-28 22:58:59 +0900

    Initial commit

---
+++ SwiftUI_GridAlbum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_GridAlbum
+//
+//  Created by Chuljin Hwang on 2022/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_GridAlbum/SwiftUI_GridAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GridAlbumApp.swift
+//  SwiftUI_GridAlbum
+//
+//  Created by Chuljin Hwang on 2022/01/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GridAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
